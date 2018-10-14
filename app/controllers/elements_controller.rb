7dbcbad4205d4ba15817a42441180a4866606778
Bui Minh Thu <buiminhthuk55@gmail.com>
2018-10-14 17:49:48 +0900

    element edit

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -12,6 +12,16 @@ class ElementsController < ApplicationController
     redirect_to elements_path
   end
 
+  def edit
+    @element = Element.find params[:id]
+  end
+
+  def update
+    @element = Element.find params[:id]
+    @element.update_attributes element_params
+    redirect_to edit_element_path(@element)
+  end
+
   private
   def element_params
     params.require(:element).permit(:name, :selector_type, :selector_value)
