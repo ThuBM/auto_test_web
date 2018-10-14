b25eac9cd7de6d1847f50b88fa329aad0bc1cd91
Bui Minh Thu <buiminhthuk55@gmail.com>
2018-10-14 17:46:37 +0900

    elemements new

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -2,4 +2,18 @@ class ElementsController < ApplicationController
   def index
     @elements = Element.all
   end
+
+  def new
+    @element = Element.new
+  end
+
+  def create
+    Element.create element_params
+    redirect_to elements_path
+  end
+
+  private
+  def element_params
+    params.require(:element).permit(:name, :selector_type, :selector_value)
+  end
 end
