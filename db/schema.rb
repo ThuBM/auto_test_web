5da2d460b93731f643c5c3e844416e1828a6f58b
Bui Minh Thu <buiminhthuk55@gmail.com>
2018-10-14 12:44:23 +0900

    003 create GroupAction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_14_033828) do
+ActiveRecord::Schema.define(version: 2018_10_14_034339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_10_14_033828) do
     t.string "name"
     t.integer "selector_type"
     t.text "selector_value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "group_actions", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
